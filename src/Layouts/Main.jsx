8628726fd4df4c2b83f0c73dc33ec1b9a57ff9a6
AbdullahWB <abdullah917828@gmail.com>
2023-06-07 18:14:12 +0600

    make footer

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navbar from '../Shared/Navbar';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Shared/Footer';
 
 const Main = () => {
     return (
         <div>
             <Navbar />
-            <Outlet />
+            <div className='min-h-[calc(100vh-1207px)]'>
+                <Outlet />
+            </div>
+            <Footer />
         </div>
     );
 };
