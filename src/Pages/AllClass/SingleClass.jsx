b3cfc4a34eba8ab92121f28fbfe5a9e1f8c92e55
AbdullahWB <abdullah917828@gmail.com>
2023-06-11 21:09:33 +0600

    make single classes add to db

--- src/Pages/AllClass/SingleClass.jsx
+++ src/Pages/AllClass/SingleClass.jsx
@@ -1,24 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useAuth from '../../Hook/useAuth';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
 
 const SingleClass = ({ item }) => {
-    const {user} = useAuth()
+    const { user } = useAuth()
     const { instructorId, description, price, availableSeats, image, instructorName, className, _id } = item || {};
     const navigate = useNavigate()
+    const location = useLocation()
+    const [addClass, SetAddClass] = useState(false)
 
     const handleAddToCart = (item) => {
         if (!user) {
-            Swal.fire(
-                'Login First',
-                'You Have to Log in First To View Details',
-                'question'
-            )
+            Swal.fire({
+                title: 'Login First',
+                text: 'You Have to Log in First To View Details',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Login Now',
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    navigate('/login', {state: {from: location}})
+                }
+            })
             navigate('/login');
             return;
         } else {
-            console.log(item);
+            if (user) {
+                const addItem = {addClassId: _id, className, instructorName, image, description, price, studentEmail: user.email}
+                fetch('http://localhost:5000/classes', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json',
+                    },
+                    body: JSON.stringify(addItem),
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.insertedId) {
+                            Swal.fire({
+                                position: 'top-end',
+                                icon: 'success',
+                                title: 'Your class has been successfully added',
+                                showConfirmButton: false,
+                                timer: 1500
+                            })
+                        }
+                    })
+            }
         }
     };
 
@@ -31,7 +62,7 @@ const SingleClass = ({ item }) => {
                     <p className='tracking-widest'>{instructorName}</p>
                     <p className='hover:text-primary'>Available Seats: {availableSeats}</p>
                     <p className='text-xl font-medium'>Course Price: {price}$</p>
-                    <button onClick={()=>handleAddToCart(item)} className="btn btn-primary mt-2 rounded-full border-2 hover:bg-transparent text-white hover:text-primary">Add Classes</button>
+                    <button disabled={addClass} onClick={() => handleAddToCart(item)} className="btn btn-primary mt-2 rounded-full border-2 hover:bg-transparent text-white hover:text-primary">Add Classes</button>
                     <button className="btn btn-outline btn-primary rounded-full border-2">Instructor</button>
                 </div>
             </div>
