ed3357d0f44b82741bcead8af43f138807fcbebc
AbdullahWB <abdullah917828@gmail.com>
2023-06-14 23:40:02 +0600

    add a condition in cart bg color

--- src/Pages/AllClass/SingleClass.jsx
+++ src/Pages/AllClass/SingleClass.jsx
@@ -58,17 +58,18 @@ const SingleClass = ({ item }) => {
 
     return (
         <>
-            <div className="card w-full rounded-none bg-base-100 shadow-xl">
+            <div className={`card w-full rounded-none bg-${availableSeats === 0 ? 'bg-primary' : 'base-100'} shadow-xl`}>
                 <figure><img className='w-full h-[270px] object-cover' src={image} alt="" /></figure>
                 <div className="card-body text-center">
                     <h2 className="text-2xl text-secondary font-semibold tracking-widest">{className}</h2>
                     <p className='tracking-widest'>{instructorName}</p>
                     <p className='hover:text-primary'>Available Seats: {availableSeats}</p>
                     <p className='text-xl font-medium'>Course Price: {price}$</p>
-                    <button disabled={userType?.admin || userType?.instructor || availableSeats<0} onClick={() => handleAddToCart(item)} className="btn btn-primary mt-2 rounded-full border-2 hover:bg-transparent text-white hover:text-primary">Add Classes</button>
+                    <button disabled={userType?.admin || userType?.instructor || availableSeats < 0} onClick={() => handleAddToCart(item)} className="btn btn-primary mt-2 rounded-full border-2 hover:bg-transparent text-white hover:text-primary">Add Classes</button>
                     <button className="btn btn-outline btn-primary rounded-full border-2">Instructor</button>
                 </div>
             </div>
+
         </>
     );
 };
